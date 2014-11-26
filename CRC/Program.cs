5c4f58fed8b153ce822494a5eeed1c8ec3b68b57
Sateanu <andrei.sateanu@gmail.com>
2014-11-26 23:04:19 +0200

    Init

---
+++ CRC/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String mess = "";
+            String gener = "";
+            Console.WriteLine("Message: ");
+            mess = Console.ReadLine();
+            Console.WriteLine("Generator: ");
+            gener = Console.ReadLine();
+            string newMess = CRC(mess, gener);
+            Console.WriteLine(newMess);
+            Console.WriteLine("Check Message: ");
+            newMess = Console.ReadLine();
+            if (CRCCheck(newMess, gener) == true)
+                Console.WriteLine("Message checked sucessfully.");
+            else Console.WriteLine("Message check failed.");
+        }
+        public static string CRC(string message, string generator)
+        {
+            int l = generator.Length;
+            string newMess = message;
+            while (l > 1)
+            {
+                message += "0";
+                l--;
+            }
+            newMess += CRCGetValue(message, generator);
+            return newMess;
+        }
+
+        public static int xor(char a, char b)
+        {
+            if (a != b)
+                return 1;
+            return 0;
+        }
+
+
+        public static String CRCGetValue(String message, String generator)
+        {
+            int i = 0;
+            char[] newMess = message.ToCharArray();
+            while (i < newMess.Length - generator.Length + 1)
+            {
+                if (newMess[i] == '1' || i == 0)
+                {
+                    int j = 0;
+                    while (j < generator.Length)
+                    {
+                        if (xor(newMess[i + j], generator[j]) == 1)
+                            newMess[i + j] = '1';
+                        else newMess[i + j] = '0';
+                        j++;
+                    }
+                    if (i == 0 && newMess[i] != '1')
+                        i++;
+                }
+                else i++;
+            }
+            message = new string(newMess);
+            //System.out.println(mess.substring(mess.length() - gener.length() +1));
+            return message.Substring(message.Length - generator.Length + 1);
+        }
+
+        public static bool CRCCheck(String message, String generator)
+        {
+            String checkValue = CRCGetValue(message, generator);
+            String s = "";
+            int i = 0;
+            while (i < generator.Length - 1)
+            {
+                s += "0";
+                i++;
+            }
+
+            return checkValue == s;
+        }
+
+    }
+}
---
+++ HamminCode/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HamminCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data: ");
+            string cod = Console.ReadLine();
+            Console.WriteLine("Hamming Code: ");
+            Console.WriteLine(hamming(cod));
+            Console.WriteLine("Check Hamming Code: ");
+            String s = Console.ReadLine();
+            CheckHamminCode(ref s);
+        }
+
+        private static void CheckHamminCode(ref string code)
+        {
+            int k;
+            if (Math.Floor(Math.Log(code.Length, 2)) == Math.Log(code.Length, 2))
+                k = (int)Math.Floor(Math.Log(code.Length, 2));
+            else k = (int)Math.Floor(Math.Log(code.Length, 2)) + 1;
+            int[] p = ParityBits(code, k);
+            int check = 0;
+            for (int i = 0; i < k; i++)
+            {
+                if (p[i] % 2 != 0)
+                    check += (int)Math.Pow(2, i);
+            }
+            if (check != 0)
+            {
+                char[] x = code.ToArray();
+                Console.WriteLine(string.Format("The {0} bit is incorrect!!", check));
+                if (code[check - 1] == '0')
+                    x[check - 1] = '1';
+                else
+                    x[check - 1] = '0';
+
+                code = new string(x);
+                Console.WriteLine("The code was fixed:" + code );
+            }
+            else Console.WriteLine("Correct code!");
+        }
+
+
+        private static string hamming(string cod)
+        {
+            int k = 0;
+            while (Math.Pow(2, k) < cod.Length + k + 1)
+                k++;
+            string newCod = "2";
+            int i = 2;
+            int j = 0;
+            int a = 1;
+
+            while (i <= cod.Length + k)
+            {
+                if (i == Math.Pow(2, a))
+                {
+                    newCod += "2";
+                    a++;
+                }
+                else
+                {
+                    newCod += cod[j];
+                    j++;
+                }
+                i++;
+            }
+            int[] p = ParityBits(newCod, k);
+            char[] c = newCod.ToArray();
+            newCod = "";
+
+            for (i = 0; i < k; i++)
+            {
+                if (p[i] % 2 == 0)
+                    c[(int)Math.Pow(2, i) - 1] = '0';
+                else c[(int)Math.Pow(2, i) - 1] = '1';
+            }
+
+            for (i = 0; i < c.Length; i++)
+                newCod += c[i];
+
+            return newCod;
+        }
+
+        public static int[] ParityBits(String newCod, int k)
+        {
+            int[] p = new int[k];
+            int i, j;
+            for (i = 1; i <= k; i++)
+            {
+                int p2 = (int)Math.Pow(2, i - 1);
+                j = p2 - 1;
+                while (j < newCod.Length)
+                {
+                    int l = 0;
+                    while (l < p2 && j < newCod.Length)
+                    {
+                        if (newCod[j] == '1')
+                            p[i - 1]++;
+                        l++;
+                        j++;
+                    }
+                    j += p2;
+                }
+            }
+            return p;
+        }
+    }
+}
